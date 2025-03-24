3bc36f8fa640594c7a685de1e1bc26f1f4d52c8a
srimanvd <srimanvd@gmail.com>
2025-03-24 14:09:21 -0400

    Add files via upload

---
+++ DataEngineering-Project1Queries.sql
@@ -0,0 +1,20 @@
+
+WITH CustomerAgent AS (
+    SELECT CM.SupportAgent, C.CustomerID
+    FROM [dbo].[ConversationMessages] AS CM
+    RIGHT JOIN [dbo].[Conversations] AS C
+      ON CM.ConversationID = C.ConversationID
+)
+
+SELECT DISTINCT(CA.SupportAgent), O.PaymentStatus
+FROM CustomerAgent AS CA
+RIGHT JOIN [dbo].[Orders] AS O
+ON CA.CustomerID = O.CustomerID
+WHERE O.PaymentStatus = 'Failed' AND CA.SupportAgent != 'NO SUPPORT AGENT'
+
+
+SELECT C.CustomerID, (C.FirstName + ' ' + C.LastName) AS Name, C.PrimaryEmailAddress, (AE.EmailAddress) AS AlternateEmailAddress
+FROM [dbo].[AlternateEmails] AS AE
+RIGHT JOIN [dbo].[Customer] AS C
+ON AE.CustomerID = C.CustomerID
+WHERE EmailAddress != 'None'
